0fa63d712ceb00e48977b758afb22f2ac07c9ba0
davidepanidev <102189224+davidepanidev@users.noreply.github.com>
2023-02-11 10:37:01 +0100

    [presentation - test] added FavouriteCoinsViewModelTest.kt tests

--- presentation/src/test/java/com/cointrend/presentation/ui/favouritecoins/FavouriteCoinsViewModelTest.kt
+++ presentation/src/test/java/com/cointrend/presentation/ui/favouritecoins/FavouriteCoinsViewModelTest.kt
@@ -115,6 +115,66 @@ class FavouriteCoinsViewModelTest : BaseCoroutineTestWithTestDispatcherProviderA
                 toIndex = any()
             )
         }
+        confirmVerified(reorderFavouriteCoinUseCase)
+        expectThat(cut.state.state).isEqualTo(CoinsListUiState.Idle)
+    }
+
+    @Test
+    fun `when onCoinPositionReordered is called two times sequentially in short time, reorderFavouriteCoinUseCase is called only the second time`() = runTest {
+        coEvery { reorderFavouriteCoinUseCase(any(), any()) } returns Result.success(Unit)
+
+        val expectedId = "btc"
+        val firstCallExpectedToIndex = 1
+        val secondCallExpectedToIndex = 2
+
+
+        cut.onCoinPositionReordered(
+            coinId = expectedId,
+            fromIndex = 0,
+            toIndex = firstCallExpectedToIndex
+        )
+
+        expectThat(cut.state.favouriteCoinsList).isEqualTo(
+            persistentListOf(
+                expectedCoinWithMarketDataEth,
+                expectedCoinWithMarketDataBtc,
+                expectedCoinWithMarketDataUsdc,
+                expectedCoinWithMarketDataUsdt,
+                expectedCoinWithMarketDataSol
+            )
+        )
+
+        advanceTimeBy(
+            FavouriteCoinsViewModel.delayBeforeCallingReorderFavouriteCoinUseCase -
+                    (FavouriteCoinsViewModel.delayBeforeCallingReorderFavouriteCoinUseCase / 2)
+        )
+
+        cut.onCoinPositionReordered(
+            coinId = expectedId,
+            fromIndex = firstCallExpectedToIndex,
+            toIndex = secondCallExpectedToIndex
+        )
+
+        expectThat(cut.state.favouriteCoinsList).isEqualTo(
+            persistentListOf(
+                expectedCoinWithMarketDataEth,
+                expectedCoinWithMarketDataUsdc,
+                expectedCoinWithMarketDataBtc,
+                expectedCoinWithMarketDataUsdt,
+                expectedCoinWithMarketDataSol
+            )
+        )
+
+        advanceUntilIdle()
+
+
+        coVerify(exactly = 1) {
+            reorderFavouriteCoinUseCase(
+                coinId = expectedId,
+                toIndex = secondCallExpectedToIndex
+            )
+        }
+        confirmVerified(reorderFavouriteCoinUseCase)
         expectThat(cut.state.state).isEqualTo(CoinsListUiState.Idle)
     }
 
