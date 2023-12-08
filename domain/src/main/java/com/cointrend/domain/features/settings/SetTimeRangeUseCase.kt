1ce4ea64e7879eecff4d66e680770d3b7986ae00
davidepanidev <102189224+davidepanidev@users.noreply.github.com>
2023-12-08 17:56:39 +0100

    [domain] added call to GetSettingsConfigurationFlowUseCase in SetTimeRangeUseCase so that every time the TimeRange is updated, the GlobalSettingsConfiguration is updated accordingly

--- domain/src/main/java/com/cointrend/domain/features/settings/SetTimeRangeUseCase.kt
+++ domain/src/main/java/com/cointrend/domain/features/settings/SetTimeRangeUseCase.kt
@@ -1,14 +1,27 @@
 package com.cointrend.domain.features.settings
 
 import com.cointrend.domain.models.TimeRange
+import kotlinx.coroutines.flow.last
 import javax.inject.Inject
 
 class SetDefaultTimeRangeUseCase @Inject constructor(
-    private val settingsRepository: SettingsRepository
+    private val settingsRepository: SettingsRepository,
+    private val getSettingsConfigurationFlowUseCase: GetSettingsConfigurationFlowUseCase
 ) {
 
     suspend operator fun invoke(timeRange: TimeRange): Result<Unit> {
-        return settingsRepository.setDefaultTimeRange(timeRange = timeRange)
+        val result = settingsRepository.setDefaultTimeRange(timeRange = timeRange)
+
+        return if (result.isSuccess) {
+            try {
+                // Before returning, getSettingsConfigurationFlowUseCase() is called so that
+                // the GlobalSettingsConfiguration is sure to be updated immediately.
+                getSettingsConfigurationFlowUseCase().last()
+                result
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        } else result
     }
 
 }
--- domain/src/main/java/com/cointrend/domain/features/settings/models/SettingsConfiguration.kt
+++ domain/src/main/java/com/cointrend/domain/features/settings/models/SettingsConfiguration.kt
@@ -3,9 +3,8 @@ package com.cointrend.domain.features.settings.models
 import com.cointrend.domain.models.Currency
 import com.cointrend.domain.models.Ordering
 import com.cointrend.domain.models.TimeRange
-import javax.inject.Singleton
 
-@Singleton
+
 data class SettingsConfiguration(
     val currency: Currency,
     val ordering: Ordering,
