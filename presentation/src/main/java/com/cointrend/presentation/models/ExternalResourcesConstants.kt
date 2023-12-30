c8902f8808826a6697f43471470e839ac617499c
davidepanidev <102189224+davidepanidev@users.noreply.github.com>
2023-12-30 11:08:12 +0100

    [presentation] Added constant external repository links to ExternalResourcesConstants file

---
+++ presentation/src/main/java/com/cointrend/presentation/models/ExternalResourcesConstants.kt
@@ -0,0 +1,9 @@
+package com.cointrend.presentation.models
+
+const val REPOSITORY_BASE_URL = "https://github.com/CoinTrend"
+const val REPOSITORY_URL = "$REPOSITORY_BASE_URL/CoinTrend"
+const val REPOSITORY_RELEASES_URL = "$REPOSITORY_URL/releases"
+const val REPOSITORY_ISSUES_URL = "$REPOSITORY_URL/issues"
+const val REPOSITORY_DONATE_URL = "$REPOSITORY_BASE_URL#support"
+
+const val SUPPORT_EMAIL = "cointrend.info@gmail.com"
