feb2b893de9e182fed2574db34c68d0271042fcc
davidepanidev <102189224+davidepanidev@users.noreply.github.com>
2025-05-29 11:03:52 +0200

    [presentation] Handled new edge to edge window

--- presentation/src/main/java/com/cointrend/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/cointrend/presentation/theme/Theme.kt
@@ -2,10 +2,13 @@ package com.cointrend.presentation.theme
 
 import android.app.Activity
 import android.os.Build
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -69,7 +72,6 @@ fun CoinTrendTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
--- presentation/src/main/java/com/cointrend/presentation/ui/coindetail/CoinDetailScreen.kt
+++ presentation/src/main/java/com/cointrend/presentation/ui/coindetail/CoinDetailScreen.kt
@@ -3,12 +3,41 @@ package com.cointrend.presentation.ui.coindetail
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExitTransition
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Grade
 import androidx.compose.material.icons.outlined.Grade
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -19,6 +48,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import com.cointrend.presentation.R
@@ -29,7 +59,12 @@ import com.cointrend.presentation.customcomposables.LineChart
 import com.cointrend.presentation.customcomposables.SegmentText
 import com.cointrend.presentation.customcomposables.SegmentedControl
 import com.cointrend.presentation.customcomposables.sharedelements.SharedElement
-import com.cointrend.presentation.models.*
+import com.cointrend.presentation.models.COIN_DETAIL_PARAMETER
+import com.cointrend.presentation.models.COIN_DETAIL_SCREEN_KEY
+import com.cointrend.presentation.models.CoinMarketChartState
+import com.cointrend.presentation.models.CoinMarketDataState
+import com.cointrend.presentation.models.CoinUiItem
+import com.cointrend.presentation.models.Screen
 import com.cointrend.presentation.theme.StocksDarkBackgroundTranslucent
 import com.cointrend.presentation.theme.StocksDarkPrimaryText
 import com.cointrend.presentation.theme.StocksDarkSecondaryText
@@ -94,11 +129,14 @@ fun CoinDetailScreen(
         snackbarHost = { SnackbarHost(snackbarHostState) },
     ) { innerPadding ->
 
-
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+                .padding(
+                    top = innerPadding.calculateTopPadding(),
+                    start = innerPadding.calculateStartPadding(LayoutDirection.Ltr),
+                    end = innerPadding.calculateEndPadding(LayoutDirection.Ltr)
+                ),
             contentPadding = PaddingValues(top = 16.dp, bottom = 32.dp),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start,
