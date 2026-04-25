a667901ef7c97f3e7df5df340b6e298b45c866ae
davidepanidev <102189224+davidepanidev@users.noreply.github.com>
2026-04-25 19:25:36 +0200

    [app] v1.3.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         minSdk = 23
         targetSdk = 36
 
-        versionCode = 10303
-        versionName = "1.3.3"
+        versionCode = 10304
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
